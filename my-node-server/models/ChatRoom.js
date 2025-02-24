d044c71f86d058b6e2179cf7c2b4c920ce4d1889
Geeemi <86919478+JimmyDevvvvv@users.noreply.github.com>
2025-02-24 10:05:18 +0200

    sockets kinda done
    Sockeeeeeeeeeeeeets

--- my-node-server/models/ChatRoom.js
+++ my-node-server/models/ChatRoom.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 
 const chatRoomSchema = new mongoose.Schema({
-    studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    driverId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    studentIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }],
+    driverIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }],
     messages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }]
 }, { timestamps: true });
 
